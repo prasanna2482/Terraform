1588a2e9f3beff6d7b4861a6040053fb722ef57b
Prasanna R <prasanna.r@opsera.io>
2025-03-13 19:12:47 +0530

    Update vars.tf

--- code/06-create-s3/vars.tf
+++ code/06-create-s3/vars.tf
@@ -1,5 +1,5 @@
 # Input variable: S3 bucket name
 variable "bucket_name" {
-  description = "The name of the S3 bucket. Must be globally unique."
-  default     = "terraform-state-my-bucket"
+  description = "Prasanna-S3-Terraform-Test"
+  default     = "Terraform Test S3"
 }
