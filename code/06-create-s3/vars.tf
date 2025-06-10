aeaac45dd73a151fe307589b9c5699849328c34f
Prasanna R <prasanna.r@opsera.io>
2025-06-10 10:29:47 +0530

    Update vars.tf

--- code/06-create-s3/vars.tf
+++ code/06-create-s3/vars.tf
@@ -1,5 +1,5 @@
 # Input variable: S3 bucket name
 variable "bucket_name" {
-  description = "s3-test-terraformnew"
-  default     = "prasanna-terraform"
+  description = "s3-test-terraformnew1"
+  default     = "prasannas-terraform"
 }
